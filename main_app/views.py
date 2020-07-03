5854a68886d134dd94120b0e524ec5115e2cec53
Percy <percy.pjc@gmail.com>
2020-07-03 17:24:58 -0400

    Add base html

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .seed import crazybones
 
 # Create your views here.
-def home(req):
-    return HttpResponse('Welcome to CrazyBones!')
+
+
+def home(request):
+    return render(request, 'home.html')
+
 
 def seed(req):
     # for cb in crazybones:
-        # add to model and save
+    # add to model and save
     pass
-    
