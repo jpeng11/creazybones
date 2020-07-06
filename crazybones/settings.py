5a9730c5bd5135005ff4f7cf8fb8f2d9990f35aa
Percy <percy.pjc@gmail.com>
2020-07-06 17:17:32 -0400

    Add templates dir in setting.py

--- crazybones/settings.py
+++ crazybones/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'crazybones.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'main_app/Templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
