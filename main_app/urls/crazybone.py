f5e50b1fbbf276489952e74c20e91900d03c0e70
Albatrooss <robillardtim@gmail.com>
2020-07-06 12:44:56 -0400

    added comments to cb details page

--- main_app/urls/crazybone.py
+++ main_app/urls/crazybone.py
@@ -3,4 +3,7 @@ from ..views import crazybone
 
 urlpatterns = [
     path('<int:cb_id>', crazybone.detail, name='cb_detail'),
+    path('<int:cb_id>/comment', crazybone.add_comment, name='add_comment'),
+    path('<int:cb_id>/comment/<int:comment_id>/remove/', crazybone.remove_comment, name='remove_comment'),
+    path('<int:cb_id>/comment/<int:comment_id>/update/', crazybone.update_comment, name='update_comment'),
 ]
