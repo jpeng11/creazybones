6d1c26ad5a6ef446990ff5bbf932c6bd4dfe8202
Ibrahim <ibrahimalhanich@gmail.com>
2020-07-07 13:17:13 -0300

    Fixed stuff again

--- main_app/urls/urls.py
+++ main_app/urls/urls.py
@@ -3,10 +3,10 @@ from ..views import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('friends/', include('main_app.urls.friends')),
     #path('profile/', include('main_app.urls.profile')),
     path('accounts/', include('main_app.urls.accounts')),
     path('crazybone/', include('main_app.urls.crazybone')),
     path('trades/', include('main_app.urls.trades')),
     path('seed/', views.seed, name='seed'),
-    path('friends/', include('main_app.urls.friends')),
 ]
