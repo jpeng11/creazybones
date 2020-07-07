68eee55e08c78580bb837bd6b54f9f5c2817b59b
Albatrooss <robillardtim@gmail.com>
2020-07-07 14:48:43 -0400

    updated profile page

--- main_app/views/profile.py
+++ main_app/views/profile.py
@@ -9,11 +9,18 @@ from ..models import Profile, Crazybone
 @login_required
 def user_profile(request, user_id):
     user = User.objects.get(id=user_id)
-    user_cb = User.objects.get(id=user_id).profile.cb.all()
-    cb_user_doesnot_own = Crazybone.objects.exclude(
-        id__in=user_cb.values_list('id'))
+    # user_cb = User.objects.get(id=user_id).profile.cb.all()
+    user_cb = user.profile.cb.all()
+    all_cb = Crazybone.objects.all().order_by('id')
+    # cb_user_doesnot_own = Crazybone.objects.exclude(
+    #     id__in=user_cb.values_list('id'))
 
-    return render(request, 'profile/detail.html', {'user': user, 'user_cb': user_cb, 'cb_user_doesnot_own': cb_user_doesnot_own})
+    return render(request, 
+        'profile/detail.html', {
+            'user': user, 
+            'user_cb': user_cb, 
+            #'cb_user_doesnot_own': cb_user_doesnot_own,
+            'all_cb': all_cb})
 
 
 class profileUpdate(UpdateView):
