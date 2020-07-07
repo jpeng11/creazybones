fb133ce40d190977fc6e0c141041af313aeebac6
Ibrahim <ibrahimalhanich@gmail.com>
2020-07-07 19:38:31 -0300

    Notifications completed

--- main_app/views/notifications.py
+++ main_app/views/notifications.py
@@ -10,8 +10,26 @@ def index(req):
     return render(req, 'notification/notification.html', {'my_notif': my_notif})
 
 @login_required
-def acceptrequest(req, pk):
-    return HttpResponse("Accepted request")
+def acceptrequest(req, pk, noti_type):
+    sent_from = Profile.objects.get(user__id = pk)
+    sent_to = Profile.objects.get(user__id = req.user.id)
+    if(noti_type == 'F'):
+        try:
+            FriendList.objects.create(user=sent_from, myId=sent_to)
+            this_noti = Notification.objects.filter(noti_to = req.user.profile)
+            this_noti = this_noti.get(noti_from = sent_from)
+            this_noti.delete()
+            return redirect('notifications')
+        except:
+            return HttpResponse("Failed")
+    return HttpResponse(f"Accepted request from profile id --> {sent_from}, {sent_to}")
 
-def rejectrequest(req, pk):
-    return HttpResponse("Rejected request")
+def rejectrequest(req, pk, noti_type):
+    try:
+        this_noti = Notification.objects.filter(noti_to = req.user.profile)
+        this_noti = this_noti.get(noti_from = Profile.objects.get(user__id = pk))
+        this_noti.delete()
+        return redirect('notifications')
+    except:
+        return HttpResponse("Failed")
+    return HttpResponse(f"Rejected request from profile id --> {pk}, {noti_type}")
